eac2205c456db0332bb5d622dc7a10ff75eda34b
Pignataro67 <Pignataro67@gmail.com>
2023-10-29 13:30:29 -0700

    CSS

--- script.js
+++ script.js
@@ -7,7 +7,14 @@
     <title>Drink Water</title>
   </head>
   <body>
+  <h1>Drink Water</h1>
+    <h3>Goal: 2 Liters</h3>
 
+    <div class="cup">
+      <div class="remained" id="remained">
+        <span id="liters"></span>
+        <small>Remained</small>
+      </div>
 
     <script src="script.js"></script>
   </body>
